120e33dc42450508518cb302cd6d6972eaca9659
huynhbgiabao <huynhbgiabao977@gmail.com>
2020-02-09 15:48:22 +0700

    first commit

---
+++ ideaboard/src/components/Idea.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react'
+
+class Idea extends Component {
+
+    handleClick = () => {
+        this.props.onClick(this.props.idea.id)
+    }
+
+    render() {
+        return (
+            <div className="tile">
+                <h4 onClick={this.handleClick}>
+                    {this.props.idea.title}
+                </h4>
+                <p onClick={this.handleClick}>
+                    {this.props.idea.body}
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Idea
